9bcf1047305dae743ec6c4e80d21007d72b055d9
Aetopia <41850963+Aetopia@users.noreply.github.com>
2025-04-08 00:06:57 +0530

    Single instance.

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,14 @@
 using System;
+using System.Globalization;
+using System.Threading;
 using System.Windows;
 
-static class Program { [STAThread] static void Main() => new Application().Run(new Window()); }
+static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.InvariantCulture;
+        using (new Mutex(false, "C997F923-EB2E-4CEF-AB11-DA7BF19CD107", out var value)) if (value) new Application().Run(new Window());
+    }
+}
